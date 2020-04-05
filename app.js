f6a17f2b2a4ab3e8d7dd2c95a2c7213978a030b9
Hiroshi Omata <op.homata@gmail.com>
2020-04-05 13:00:52 +0900

    Add sidemenu

---
+++ app.js
@@ -0,0 +1,47 @@
+//ons.bootstrap();
+
+window.fn = {};
+
+window.fn.toggleMenu = function () {
+    document.getElementById('appSplitter').right.toggle();
+};
+
+window.fn.loadView = function (index) {
+    document.getElementById('appTabbar').setActiveTab(index);
+    document.getElementById('sidemenu').close();
+};
+
+window.fn.loadLink = function (url) {
+    window.open(url, '_blank');
+};
+
+window.fn.pushPage = function (page, anim) {
+    if (anim) {
+        document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title }, animation: anim });
+    } else {
+        document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title } });
+    }
+};
+
+// アクティブなタブが変わる前に発火します。
+document.addEventListener('prechange', function(event) {
+    // ラベル設定
+    document.querySelector('ons-toolbar .center')
+        .innerHTML = event.tabItem.getAttribute('label');
+});
+
+// initイベント
+document.addEventListener("init", function(event) {
+    var page = event.target;
+    if (page.id === "home-page") {
+        console.log("home-page");
+    } else if (page.id === "list-page") {
+        console.log("list-page");
+    } else if (page.id === "map-page") {
+        console.log("map-page");
+    } else if (page.id === "info-page") {
+        console.log("inso-page");
+    } else if (page.id === "setting-page") {
+        console.log("setting-page");
+    }
+});
